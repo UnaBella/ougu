786b434fe6ab46d9ea7484cf7d13d7434da3d250
una <tenglf@xiaoman.com>
2018-01-12 11:16:09 +0800

    头部悬停改变

--- ougu/js/index.js
+++ ougu/js/index.js
@@ -6,7 +6,6 @@ window.onscroll = function () {
 				_header.className = 'header';
 			}
 		};
-
 		function getScrollTop()
 		{
 			var scrollTop=0;
@@ -19,4 +18,35 @@ window.onscroll = function () {
 				scrollTop=document.body.scrollTop;
 			}
 			return scrollTop;
+		}
+		function getE(id) {
+			return document.getElementById(id);
+		}
+		var clic = getE("clic");
+		var clip = getE("clip");
+		var chh = getE("chh");
+		var phh = getE("phh");
+		clic.onmouseover = function  () {
+			chh.style.display = "block";
+		}
+		clic.onmouseout = function() {
+			chh.style.display = "none";
+		}
+		chh.onmouseover = function() {
+			this.style.display = "block";
+		}
+		chh.onmouseout = function() {
+			this.style.display = "none";
+		}
+		clip.onmouseover = function  () {
+			phh.style.display = "block";
+		}
+		clip.onmouseout = function() {
+			phh.style.display = "none";
+		}
+		phh.onmouseover = function() {
+			this.style.display = "block";
+		}
+		phh.onmouseout = function() {
+			this.style.display = "none";
 		}
