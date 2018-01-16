d69126e84867525ae375128c05e612d4a446255b
una <tenglf@xiaoman.com>
2018-01-16 16:57:45 +0800

    banner 及字号 2560

--- ougu/js/header.js
+++ ougu/js/header.js
@@ -1,30 +1,30 @@
 var a  = '<div class="headerhover" id="chh">'+
 						'<ul class="hhul">'+
-							'<a href="C01_cuiJ.html"><li>'+
+							'<li><a href="C01_cuiJ.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/customization/01_phone.png"/></div>'+
 								'<p class="p">崔健手机BLUE BONE2</p>'+
-							'</li></a>'+
-							'<a href="C02_jeep.html"><li>'+
+							'</a></li>'+
+							'<li><a href="C02_jeep.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/customization/01_jeep.png"/></div>'+
 								'<p class="p">JEEP70周年手机</p>'+
-							'</li></a>'+
-							'<a href="C03_qingHG.html"><li>'+
+							'</a></li>'+
+							'<li><a href="C03_qingHG.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/customization/01_qhc.png"/></div>'+
 								'<p class="p">青花馆手机</p>'+
-							'</li></a>'+
-							'<a href="C04_cuiJYSP.html"><li>'+
+							'</a></li>'+
+							'<li><a href="C04_cuiJYSP.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/customization/01_other.png"/></div>'+
 								'<p class="p">崔健衍生系列</p>'+
-							'</li></a>'+
-							'<a href="C05_ifi.html"><li>'+
+							'</a></li>'+
+							'<li><a href="C05_ifi.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/customization/01_ifi.png"/></div>'+
 								'<p class="p">智能I-FI</p>'+
-							'</li></a>'+
+							'</a></li>'+
 						'</ul>'+
 					'</div>'+
 					'<div class="headerhover project" id="phh">'+
 						'<ul class="hhul projectul">'+
-							'<a href="P01_Y01_cengY.html"><li>'+
+							'<li><a href="P01_Y01_cengY.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/product/01_styy.png"/></div>'+
 								'<p class="p">影音视听</p>'+
 								'<div class="t active"><a href="P01_Y01_cengY.html">层云系列音响</a></div>'+
@@ -32,28 +32,28 @@ var a  = '<div class="headerhover" id="chh">'+
 								'<div class="t"><a href="P01_Y03_XBox.html">X-BOX耳机</a></div>'+
 								'<div class="t"><a href="P01_Y04_tangMC.html">汤姆逊电子管音响</a></div>'+
 								'<div class="t"><a href="P01_Y05_suiJG.html">崔建国音响</a></div>'+
-							'</li></a>'+
-							'<a href="P02_C01_lanY.html"><li>'+
+							'</a></li>'+
+							'<li><a href="P02_C01_lanY.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/product/01_clw.png"/></div>'+
 								'<p class="p">车联网</p>'+
 								'<div class="t"><a href="P02_C01_lanY.html">蓝牙耳机</a></div>'+
-							'</li></a>'+
-							'<a href="P03_S01_ziPSQ.html"><li>'+
+							'</a></li>'+
+							'<li><a href="P03_S01_ziPSQ.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/product/01_smcp.png"/></div>'+
 								'<p class="p">数码产品</p>'+
 								'<div class="t"><a href="P03_S01_ziPSQ.html">自拍神器</a></div>'+
 								'<div class="t"><a href="P03_S02_yingDDY.html">移动电源</a></div>'+
-							'</li></a>'+
-							'<a href="P04_S01_zingNJJ.html"><li>'+
+							'</a></li>'+
+							'<li><a href="P04_S01_zingNJJ.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/product/01_znjj.png"/></div>'+
 								'<p class="p">智能家居</p>'+
 								'<div class="t"><a href="P04_S01_zingNJJ.html">空气净化器</a></div>'+
-							'</li></a>'+
-							'<a href="P05_D01_daiBGJ.html"><li>'+
+							'</a></li>'+
+							'<li><a href="P05_D01_daiBGJ.html">'+
 								'<div class="img"><img src="http://zimeitisucai.oss-cn-beijing.aliyuncs.com/resource/ouguWebsite/img/product/01_dbgj.png"/></div>'+
 								'<p class="p">代步工具</p>'+
 								'<div class="t"><a href="P05_D01_daiBGJ.html">UNIONO双轮车</a></div>'+
-							'</li></a>'+
+							'</a></li>'+
 						'</ul>'+
 					'</div>'
 				$('.header').append(a);
